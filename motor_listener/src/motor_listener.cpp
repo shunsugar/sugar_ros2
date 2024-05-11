157cd20b1cc7d528efbd821050c419647ee83a70
shunsugar <shun.saito.6t@stu.hosei.ac.jp>
2024-05-11 10:22:01 +0000

    add motor_listener

---
+++ motor_listener/src/motor_listener.cpp
@@ -0,0 +1,23 @@
+#include "motor_listener.hpp"
+
+
+MotorListener::MotorListener() : Node("motor_listener")
+{
+    subscription_ = this->create_subscription<geometry_msgs::msg::Twist>
+    ("/cmd_vel", 10, std::bind(&MotorListener::cmd_vel_callback, this, std::placeholders::_1));
+}
+
+void MotorListener::cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg)
+{
+    RCLCPP_INFO(rclcpp::get_logger("motor_listener"), 
+    "Received cmd_vel: linear.x=%.2f, angular.z=%.2f", msg->linear.x, msg->angular.z);
+}
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<MotorListener>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
