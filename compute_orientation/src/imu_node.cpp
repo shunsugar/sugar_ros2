be193450e2759246fd638a9ae7bd8a86c8395210
shunsugar <shun.saito.6t@stu.hosei.ac.jp>
2024-05-31 14:46:22 +0000

    add compute_orientation

---
+++ compute_orientation/src/imu_node.cpp
@@ -0,0 +1,31 @@
+#include "imu_node.hpp"
+
+ImuNode::ImuNode() : Node("imu_node")
+{
+  this->declare_parameter<std::string>("imu_topic", "imu");
+  this->get_parameter("imu_topic", imu_topic_);
+  imu_pub_ = this->create_publisher<sensor_msgs::msg::Imu>(imu_topic_, 10);
+  livox_imu_sub_ = this->create_subscription<sensor_msgs::msg::Imu>(
+                   "/livox/imu", 10, std::bind(&ImuNode::computeOrientationData, this, std::placeholders::_1));
+}
+
+void ImuNode::computeOrientationData(const sensor_msgs::msg::Imu::SharedPtr msg)
+{
+  RCLCPP_INFO(rclcpp::get_logger(imu_topic_), "Received /livox/imu");
+  //publishMsg(msg);
+}
+/*
+void ImuNode::publishMsg(const sensor_msgs::msg::Imu::SharedPtr msg)
+{
+  //RCLCPP_INFO(rclcpp::get_logger(imu_topic_), "Published %s", imu_topic_);
+  imu_pub_->publish(msg);
+}
+*/
+int main(int argc, char* argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<ImuNode>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
