f0e607784734778c564451037d6eb954425a88c8
shunsugar <shin.ioaoi@gmail.com>
2025-05-12 05:54:54 +0000

    Add parameters.

--- sensor_utils/include/wheel_imu_odom.hpp
+++ sensor_utils/include/wheel_imu_odom.hpp
@@ -17,7 +17,7 @@ public:
 private:
   void imuCallback(const sensor_msgs::msg::Imu::SharedPtr msg);
   void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
-  double getYawFromQuaternion(const geometry_msgs::msg::Quaternion &q);
+  static double getYawFromQuaternion(const geometry_msgs::msg::Quaternion &q);
 
   rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
@@ -27,17 +27,28 @@ private:
   std::string imu_topic_;
   std::string odom_topic_;
   std::string fused_odom_topic_;
+  
   std::string odom_header_frame_;
   std::string odom_child_frame_;
   std::string TF_header_frame_;
   std::string TF_child_frame_;
+  
+  double scale_factor_;
   bool publish_odom_;
   bool publish_TF_;
   bool debug_;
 
+  rclcpp::Time prev_time_;
   double yaw_;
-  double x_;
-  double y_;
+  double position_x_;
+  double position_y_;
+  double orientation_x_;
+  double orientation_y_;
+  double orientation_z_;
+  double orientation_w_;
+  double ang_vel_x_;
+  double ang_vel_y_;
+  double ang_vel_z_;
 };
 
 #endif // WHEEL_IMU_ODOM_H
--- sensor_utils/src/wheel_imu_odom.cpp
+++ sensor_utils/src/wheel_imu_odom.cpp
@@ -5,10 +5,13 @@ OdomFusionNode::OdomFusionNode() : Node("wheel_imu_odom")
   imu_topic_ = this->declare_parameter<std::string>("imu_topic", "/imu");
   odom_topic_ = this->declare_parameter<std::string>("odom_topic", "/odom");
   fused_odom_topic_ = this->declare_parameter<std::string>("fused_odom_topic", "/odom/wheel_imu");
+  
   odom_header_frame_ = this->declare_parameter<std::string>("odom_header_frame", "odom");
   odom_child_frame_ = this->declare_parameter<std::string>("odom_child_frame", "base_footprint");
   TF_header_frame_ = this->declare_parameter<std::string>("TF_header_frame", "odom");
   TF_child_frame_ = this->declare_parameter<std::string>("TF_child_frame", "base_footprint");
+  
+  scale_factor_ = this->declare_parameter<double>("scale_factor", 0.45);
   publish_odom_ = this->declare_parameter<bool>("publish_odom", true);
   publish_TF_ = this->declare_parameter<bool>("publish_TF", true);
   debug_ = this->declare_parameter<bool>("debug", false);
@@ -22,8 +25,9 @@ OdomFusionNode::OdomFusionNode() : Node("wheel_imu_odom")
   tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
   
   yaw_ = 0.0;
-  x_ = 0.0;
-  y_ = 0.0;
+  position_x_ = 0.0;
+  position_y_ = 0.0;
+  prev_time_ = this->now();
   
   RCLCPP_INFO(this->get_logger(), "Initialized wheel_imu_odom_node.");
   RCLCPP_INFO(this->get_logger(), "publish_odom: %s", publish_odom_ ? "true" : "false");
@@ -33,17 +37,25 @@ OdomFusionNode::OdomFusionNode() : Node("wheel_imu_odom")
 
 void OdomFusionNode::imuCallback(const sensor_msgs::msg::Imu::SharedPtr msg)
 {
+  orientation_x_ = msg->orientation.x;
+  orientation_y_ = msg->orientation.y;
+  orientation_z_ = msg->orientation.z;
+  orientation_w_ = msg->orientation.w;
+  ang_vel_x_ = msg->angular_velocity.x;
+  ang_vel_y_ = msg->angular_velocity.y;
+  ang_vel_z_ = msg->angular_velocity.z;
   yaw_ = getYawFromQuaternion(msg->orientation);
 }
 
 void OdomFusionNode::odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
 {
-  double dx = msg->twist.twist.linear.x * 0.02; // 20ms
-  x_ += dx * cos(yaw_);
-  y_ += dx * sin(yaw_);
+  rclcpp::Time current_time = msg->header.stamp;
+  double dt = (current_time - prev_time_).seconds();
+  prev_time_ = current_time;
 
-  tf2::Quaternion q;
-  q.setRPY(0, 0, yaw_);
+  double dx = msg->twist.twist.linear.x * dt * scale_factor_;  // best: 0.45
+  position_x_ += dx * cos(yaw_);
+  position_y_ += dx * sin(yaw_);
 
   // Construct TF
   if (publish_TF_)
@@ -52,10 +64,13 @@ void OdomFusionNode::odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
     t.header.stamp = this->get_clock()->now();
     t.header.frame_id = TF_header_frame_;
     t.child_frame_id = TF_child_frame_;
-    t.transform.translation.x = x_;
-    t.transform.translation.y = y_;
+    t.transform.translation.x = position_x_;
+    t.transform.translation.y = position_y_;
     t.transform.translation.z = 0.0;
-    t.transform.rotation = tf2::toMsg(q);
+    t.transform.rotation.x = orientation_x_;
+    t.transform.rotation.y = orientation_y_;
+    t.transform.rotation.z = orientation_z_;
+    t.transform.rotation.w = orientation_w_;
     tf_broadcaster_->sendTransform(t);
   }
 
@@ -66,18 +81,25 @@ void OdomFusionNode::odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
     fused_msg.header.stamp = this->get_clock()->now();
     fused_msg.header.frame_id = odom_header_frame_;
     fused_msg.child_frame_id = odom_child_frame_;
-    fused_msg.pose.pose.position.x = x_;
-    fused_msg.pose.pose.position.y = y_;
+    fused_msg.pose.pose.position.x = position_x_;
+    fused_msg.pose.pose.position.y = position_y_;
     fused_msg.pose.pose.position.z = 0.0;
-    fused_msg.pose.pose.orientation = tf2::toMsg(q);
-    fused_msg.twist.twist = msg->twist.twist;
+    fused_msg.pose.pose.orientation.x = orientation_x_;
+    fused_msg.pose.pose.orientation.y = orientation_y_;
+    fused_msg.pose.pose.orientation.z = orientation_z_;
+    fused_msg.pose.pose.orientation.w = orientation_w_;
+    fused_msg.twist.twist.linear.x = msg->twist.twist.linear.x;
+    fused_msg.twist.twist.linear.y = 0.0;
+    fused_msg.twist.twist.angular.x = ang_vel_x_;
+    fused_msg.twist.twist.angular.y = ang_vel_y_;
+    fused_msg.twist.twist.angular.z = ang_vel_z_;
     fused_odom_pub_->publish(fused_msg);
   }
   
   // Debugging Feature
   if (debug_)
   {
-    RCLCPP_INFO(this->get_logger(), "x: %f, y: %f, yaw: %f", x_, y_, yaw_);
+    RCLCPP_INFO(this->get_logger(), "x: %f, y: %f, yaw: %f", position_x_, position_y_, yaw_);
   }
 }
 
